08ac2824d07444f92c8e20f8d9a76c051e875328
Ianiv <ihojmanb@gmail.com>
2018-09-18 22:58:54 -0300

    segunda version

--- watering-system-IH/watering-system-IH.ino
+++ watering-system-IH/watering-system-IH.ino
@@ -6,20 +6,28 @@ const int pumpCathodePin =  7;   //pin 7 connect to the cathode of the pump
 Servo servoH;  // create servo object to control a servo
 Servo servoV;  // create servo object to control a servo
 int pinHumed1 = 0;  // pin análogo para sensor de humedad 1
-
+int pinHumed2 = 1;  // pin análogo para sensor de humedad 2
+int pinHumed3 = 2;  // pin análogo para sensor de humedad 3
 int humed1;    // variable para leer el valor del sensor 1
-
+int humed2;    // variable para leer el valor del sensor 2
+int humed3;    // variable para leer el valor del sensor 3
 int servo_humed1;
-
+int sensor_list[] = {pinHumed1, pinHumed2, pinHumed3};
 
 int umbralInf_1 = 50; // cota inferior de humedad en porcentaje sensor 1
+int umbralInf_2 = 50; // cota inferior de humedad en porcentaje sensor 2
+int umbralInf_3 = 50; // cota inferior de humedad en porcentaje sensor 
 
 int umbralSup_1 = 70; // cota superior de humedad en porcentaje sensor 1
-3
+int umbralSup_2 = 80; // cota superior de humedad en porcentaje sensor 2
+int umbralSup_3 = 80; // cota superior de humedad en porcentaje sensor 3
 int umbral_humed1;
 
 int angulo_inicial = 0;
 int angulo_1 = 30;
+int angulo_2 = 90;
+int angulo_3 = 150;
+int angulo_list[] = {angulo_1, angulo_2, angulo_3};
 
 void setup() 
 {  
@@ -36,8 +44,10 @@ regar(0);
 }
 
 
-void regar(){
-    humed1 = analogRead(pinHumed1);            // reads the value of the potentiometer  
+void regar(int j){
+  while(j < (sizeof(sensor_list)/sizeof(int))){   
+    int i=j;
+    humed1 = analogRead(sensor_list[i]);            // reads the value of the potentiometer  
     umbral_humed1 = map(humed1, 0, 1023, 100, 0);
     Serial.print("Humedad: ");
     Serial.print(umbral_humed1);
@@ -45,10 +55,12 @@ void regar(){
     delay(1000);
   
     if((umbral_humed1 >= umbralInf_1 && umbral_humed1 <= umbralSup_1) || (umbral_humed1 > umbralSup_1)){
-      Serial.print("  (bomba off)"); 
+      Serial.print("  (bomba off)");
+      i++;
+      regar(i);  
       }  
     if(umbral_humed1 < umbralInf_1){
-      servoH.write(angulo_1); 
+      servoH.write(angulo_list[i]); 
       delay(1000);
       digitalWrite(pumpAnodePin, HIGH);    
       delay(5000); 
